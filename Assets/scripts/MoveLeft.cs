9d5f4b8240cc0aebbb913ca44a3d62398a48ade4
Ronnie El-Alam <rel-alam@hotmail.com>
2015-04-23 16:29:59 +1000

    + movement to move forward + death on hitting ground

--- Assets/scripts/MoveLeft.cs
+++ Assets/scripts/MoveLeft.cs
@@ -3,36 +3,47 @@ using System.Collections;
 
 public class MoveLeft : MonoBehaviour {
 
-	Vector2 LeftSide = new Vector2(0, 0);
-	Vector2 RightSide = new Vector2(0, 0);
+	float LeftX = 0.0f;
+    float RightX = 0.0f;
+    float y = 0.0f;
 
 	Vector3 pos = new Vector3(5, 0, 0);
+    Vector3 up = new Vector3(0, 5, 0);
 	public GameObject other;
 
 
 	// Use this for initialization
 	void Start () {
-		LeftSide.x = Screen.width / 3;
-		//LeftSide.y = Screen.height / 2 - 100;
-		RightSide.x = LeftSide.x * 2;
-        
+        LeftX = Screen.width / 3;
+		
+        RightX = LeftX * 2;
+
+        y = Screen.height / 2 - 100;
 	
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-		if (Input.mousePosition.x > 0 && Input.mousePosition.x < LeftSide.x && Input.GetMouseButton(0)) {
+
+        if (Input.mousePosition.x > 0 && Input.mousePosition.x < LeftX && Input.GetMouseButton(0))
+        {
 		
 
 			//other.transform.position = pos;
             other.rigidbody.AddForce(pos);
 		}
 
-		if (Input.mousePosition.x > RightSide.x && Input.mousePosition.x < Screen.width && Input.GetMouseButton(0)) {
+        if (Input.mousePosition.x > RightX && Input.mousePosition.x < Screen.width && Input.GetMouseButton(0))
+        {
 			//other.transform.position = -pos;
             other.rigidbody.AddForce(-pos);
 				}
+
+        if (Input.mousePosition.y < y && Input.mousePosition.x > 0 && Input.GetMouseButton(0))
+        {
+            //other.transform.position = -pos;
+            other.rigidbody.AddForce(up);
+        }
 	}
 }
---
+++ Assets/scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+ 
+	}
+	// Update is called once per frame
+    void Update()
+    {
+
+        Vector3 temp = gameObject.transform.position;
+        temp.z += 4 * Time.deltaTime;
+        gameObject.transform.position = temp; // = MoveZ;
+
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.CompareTag("death"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
+
