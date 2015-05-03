aaf7214cab75297f281aa53ce5ac9934c163a772
BuildTools <unconfigured@null.spigotmc.org>
2015-05-03 17:41:21 +1000

    Renamed move left to controls +added in touch movement

--- Assets/scripts/Control.cs
+++ Assets/scripts/Control.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MoveLeft : MonoBehaviour {
+public class Control : MonoBehaviour {
 
 	float LeftX = 0.0f;
     float RightX = 0.0f;
@@ -28,22 +28,38 @@ public class MoveLeft : MonoBehaviour {
 
         if (Input.mousePosition.x > 0 && Input.mousePosition.x < LeftX && Input.GetMouseButton(0))
         {
-		
-
-			//other.transform.position = pos;
             other.rigidbody.AddForce(pos);
 		}
 
         if (Input.mousePosition.x > RightX && Input.mousePosition.x < Screen.width && Input.GetMouseButton(0))
         {
-			//other.transform.position = -pos;
             other.rigidbody.AddForce(-pos);
 				}
 
         if (Input.mousePosition.y < y && Input.mousePosition.x > 0 && Input.GetMouseButton(0))
-        {
-            //other.transform.position = -pos;
+		{
             other.rigidbody.AddForce(up);
         }
+
+		//if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+		//{
+		//	Vector2 touchPosition = Input.GetTouch(0).deltaPosition;
+		//
+		//	if (touchPosition.x > 0 && touchPosition.x < LeftX)
+		//	{
+		//		other.rigidbody.AddForce(pos);
+		//	}
+		//	
+		//	if (touchPosition.x > RightX && touchPosition.x < Screen.width)
+		//	{
+		//		other.rigidbody.AddForce(-pos);
+		//	}
+		//	
+		//	if (touchPosition.y < y && touchPosition.y > 0)
+		//	{
+		//		other.rigidbody.AddForce(up);
+		//	}
+		//
+		//}
 	}
 }
