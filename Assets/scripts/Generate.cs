d573e86a9248f91d237fe1f3b31dd0fa56258d75
Ronnie El-Alam <rel-alam@hotmail.com>
2015-05-05 23:04:34 +1000

    + added generation of obstacles

---
+++ Assets/scripts/Generate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generate : MonoBehaviour {
+
+	public GameObject obstacle;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("CreateObstacle", 1f, 3f);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void CreateObstacle()
+	{
+
+		Instantiate (obstacle);
+	}
+}
---
+++ Assets/scripts/Obstacle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+	// Use this for initialization
+
+	public Vector3 velocity = new Vector3(2, 0, 5);
+	public float range = 1;
+	void Start () {
+
+		rigidbody.velocity = velocity;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
